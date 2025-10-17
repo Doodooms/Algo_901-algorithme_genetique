3a867f49e2d343e9475e9d55bc25e7633e0a04f8
Doodooms <mailhebiauphoebus@gmail.com>
2025-10-17 11:21:47 +0200

    rajout coordonnees codees dans classe coordonnnees

--- core/Coordonnees.py
+++ core/Coordonnees.py
@@ -1,8 +1,9 @@
 import numpy as np
 
 class coordonnees:
-    def __init__(self, coordonnees: np.array):
+    def __init__(self, coordonnees: np.array, coordonnees_codees: np.array):
         self.coordonnees = coordonnees
+        self.coordonnees_codees = coordonnees_codees
 
     def __str__(self):
         return str(self.coordonnees)

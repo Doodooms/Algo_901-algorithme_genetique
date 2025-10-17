454e0aab466cd2fb45fee444cfd582c8c33f19ec
Tote <thomas.pelhatre@gmail.com>
2025-10-17 16:02:32 +0200

    Ajout de SimpleCrossover et de mutations

--- core/Coordonnees.py
+++ core/Coordonnees.py
@@ -1,13 +1,18 @@
 import numpy as np
 
+
 class coordonnees:
-    def __init__(self, coordonnees: np.array):
+    def __init__(
+        self, coordonnees: np.array  # plutôt np.ndarray je crois
+    ):  # Ca peut être utile de pouvoir instancier des coordonnes avec les coordonnes codees (pour les enfants)
+        # Du coup peut être mettre coordonnees et coordonnees_codees en Optionnal[np.ndarray]
         self.coordonnees = coordonnees
         self.coordonnees_codees = None
 
     def __str__(self):
         return f"Coordonnées : {self.coordonnees}, Coordonnées codées : {self.coordonnees_codees}"
-    
+
+
 if __name__ == "__main__":
     c = coordonnees(np.array([1.5, 2.5, 3.5]))
     print(c)
--- operators/Mutation.py
+++ operators/Mutation.py
@@ -1,15 +1,48 @@
-from core.Individu import Individu
+import numpy as np
+
+from core.Individu import individu
 
 
 class Mutation:
     """
     Classe qui permet d'effectuer une mutation sur un individu selon un certain taux de mutation
+    Marche uniquement sur un codage binaire !
+    J'ai mis deux facon de voir je sais pas laquelle vous semble la plus logique
     """
 
     def __init__(self, taux_mutation: float):
         self.taux_mutation = taux_mutation
 
-    def mutation(self, individu: Individu):
+    def mutation1(self, individu: individu):
         """
-        Fonction qui prend un individu et effectue des mutations sur ses coordonnées
+        Fonction qui prend un individu et effectue des mutations sur ses coordonnées.
+        On calcule d'abord le nombre de mutation, puis on modifie aléatoirement dans les coordonnées codées
         """
+        coordonnees_mutees = (
+            individu.coordonnees.coordonnees_codees
+        )  # On l'initalise avec les coordonnées codées intiales
+        nb_coordonnees = len(coordonnees_mutees)
+        taille_codage = len(coordonnees_mutees[0])
+        nb_cases = nb_coordonnees * taille_codage
+        nb_mutation = int(np.floor(nb_cases * self.taux_mutation))
+        cases_mutees = np.random.choice(nb_cases, nb_mutation, False)
+
+        for case in cases_mutees:
+            num_coord = case // taille_codage
+            num_case = case % taille_codage
+            coordonnees_mutees[num_coord][num_case] = (
+                1 - coordonnees_mutees[num_coord][num_case]
+            )
+            # Je sais pas vraiment encore sous quelle forme est le codage, ligne du dessus peut être à modifier
+
+    def mutation2(self, individu: individu):
+        """
+        Fonction qui prend un individu et effectue des mutations sur ses coordonnées.
+        Pour chaque case, on a la probabilité taux_mutation d'effectuer une mutation
+        """
+        for coordonne in individu.coordonnees.coordonnees_codees:
+            for i in range(len(coordonne)):
+                if (
+                    np.random.rand() <= self.taux_mutation
+                ):  # On effectue la mutation avec la proba taux_mutation
+                    coordonne[i] = 1 - coordonne[i]  # Pareil ici, peut être à modifier
